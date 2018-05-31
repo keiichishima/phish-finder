6bfd191109a691dfcdedd34562fe7b9f159f6702
Keiichi SHIMA <keiichi@iijlab.net>
2018-05-31 14:13:01 +0900

    added argument for binding address for server

--- server/server.py
+++ server/server.py
@@ -18,8 +18,21 @@ def _message_received(_client, _server, _msg):
             continue
         _server.send_message(_clients[_id], _msg)
 
-_server = WebsocketServer(5678, '127.0.0.1')
-_server.set_fn_new_client(_new_client)
-_server.set_fn_client_left(_client_left)
-_server.set_fn_message_received(_message_received)
-_server.run_forever()
+if __name__ == '__main__':
+    import argparse
+
+    _parser = argparse.ArgumentParser()
+    _parser.add_argument('-b',
+                         dest='bind_address',
+                         default='127.0.0.1',
+                         help='Bind address ("-b all" for wildcard')
+    _args = _parser.parse_args()
+
+    if _args.bind_address == 'all':
+        _args.bind_address = ''
+
+    _server = WebsocketServer(5678, _args.bind_address)
+    _server.set_fn_new_client(_new_client)
+    _server.set_fn_client_left(_client_left)
+    _server.set_fn_message_received(_message_received)
+    _server.run_forever()
