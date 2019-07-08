599a95d2fbfc485b43ccb8168513b3f28ae0d683
Keiichi SHIMA <keiichi@iijlab.net>
2019-07-08 14:30:22 +0900

    reshape command line args

--- server/server.py
+++ server/server.py
@@ -2,8 +2,6 @@
 
 from websocket_server import WebsocketServer
 
-WS_PORT=5678
-
 _clients = {}
 
 def _new_client(_client, _server):
@@ -24,16 +22,21 @@ if __name__ == '__main__':
     import argparse
 
     _parser = argparse.ArgumentParser()
-    _parser.add_argument('-b',
-                         dest='bind_address',
+    _parser.add_argument('-wshost',
+                         dest='wshost',
                          default='127.0.0.1',
-                         help='Bind address ("-b all" for wildcard')
+                         help='Bind address ("-wshost all" for wildcard')
+    _parser.add_argument('-wsport',
+                         dest='wsport',
+                         type=int,
+                         default=5678,
+                         help='Websocket server port')
     _args = _parser.parse_args()
 
-    if _args.bind_address == 'all':
-        _args.bind_address = ''
+    if _args.wshost == 'all':
+        _args.wshost = ''
 
-    _server = WebsocketServer(WS_PORT, _args.bind_address)
+    _server = WebsocketServer(_args.wsport, _args.wshost)
     _server.set_fn_new_client(_new_client)
     _server.set_fn_client_left(_client_left)
     _server.set_fn_message_received(_message_received)
